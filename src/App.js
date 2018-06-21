656f695c501202dbc9ef2bd4d25bbf0a9e75311c
shreyas2047 <shreyass.hf95@gmail.com>
2018-06-21 09:38:56 -0700

    Added image recognition api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: 'd299f4e716774f3c846c3f17a1d368d1'
+ });
 
 const particlesOptions = {
   particles: {
@@ -20,6 +25,29 @@ const particlesOptions = {
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event)=> {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = ()=> {
+    console.log('click');
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+      function(response) {
+        console.log(response);
+      },
+      function(err) {
+        // there was an error
+      }
+  );
+  }
+
   render() {
     return (
       <div className="App">
@@ -29,7 +57,9 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit} />
         {/*
         <FaceRecognition />
         */}
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = ()=> {
+const ImageLinkForm = ({onInputChange, onButtonSubmit})=> {
     return (
         <div>
             <p className=" white f3">
@@ -9,8 +9,11 @@ const ImageLinkForm = ()=> {
             </p>
             <div className ='center' >
                 <div className="pa4 form center br3 shadow-5" >
-                    <input className="f4 pa2 w-70 center" type="text" />
-                    <button className="w-30 grow f4 link ph3 pv2 dib bg-light-purple" >Detect</button>
+                    <input className="f4 pa2 w-70 center" type="text"
+                        onChange={onInputChange} />
+                    <button className="w-30 grow f4 link ph3 pv2 dib bg-light-purple"
+                        onClick={onButtonSubmit}
+                     >Detect</button>
                 </div>
             </div>
         </div>
