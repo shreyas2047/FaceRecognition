2b2149ac35b45d44dcbd6fec3b5de04e8a802f31
shreyas2047 <shreyass.hf95@gmail.com>
2018-07-08 16:44:19 -0400

    updated the server urls

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
@@ -10,9 +9,6 @@ import Signin from './components/Signin/Signin';
 import Register from './components/Register/Register';
 import './App.css';
 
-const app = new Clarifai.App({
-  apiKey: 'd299f4e716774f3c846c3f17a1d368d1'
- });
 
 const particlesOptions = {
   particles: {
@@ -26,7 +22,20 @@ const particlesOptions = {
   }
 }
 
-
+const initialState = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
+}
 class App extends Component {
   constructor() {
     super();
@@ -35,9 +44,32 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
   }
+
+  loadUser = (data)=> {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
+  }
+
+  // componentDidMount() {
+  //   fetch('http://localhost:3000')
+  //     .then(response => response.json())
+  //     .then(console.log);
+  // }
 
   calculateFaceLocation = (data)=> {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
@@ -63,18 +95,38 @@ class App extends Component {
   }
 
   onButtonSubmit = ()=> {
-    console.log('click');
     this.setState({imageUrl: this.state.input});
-    app.models.predict(
-        Clarifai.FACE_DETECT_MODEL,
-        this.state.input)
-      .then(response=> this.displayFaceBox(this.calculateFaceLocation(response)))
+    fetch('https://infinite-cliffs-71173.herokuapp.com/imageurl', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              input: this.state.input
+            })
+          })
+      .then(response => response.json())
+      .then(response=> {
+        if(response) {
+          fetch('https://infinite-cliffs-71173.herokuapp.com/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, {entries: count}))
+            })
+            .catch(console.log)
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
       .catch(err=> console.log(err));
   }
 
   onRouteChange = (route)=> {
     if(route === 'signout') {
-      this.setState({isSignedIn: false});
+      this.setState(initialState);
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
@@ -87,11 +139,17 @@ class App extends Component {
         <Particles className= "particles"
               params={particlesOptions}
         />
-        <Navigation onRouteChange={this.onRouteChange} isSignedIn={this.state.isSignedIn} />
+        <Navigation
+          onRouteChange={this.onRouteChange} 
+          isSignedIn={this.state.isSignedIn} 
+        />
         { this.state.route === 'home'
           ? <div> 
               <Logo />
-              <Rank />
+              <Rank
+                name={this.state.user.name}
+                entries={this.state.user.entries} 
+              />
               <ImageLinkForm
                 onInputChange={this.onInputChange}
                 onButtonSubmit={this.onButtonSubmit} />
@@ -101,8 +159,14 @@ class App extends Component {
             </div>
           : (
               this.state.route === 'signin' 
-              ? <Signin onRouteChange={this.onRouteChange} />
-              : <Register onRouteChange={this.onRouteChange} />
+              ? <Signin 
+                  onRouteChange={this.onRouteChange}
+                  loadUser={this.loadUser} 
+                />
+              : <Register
+                  onRouteChange={this.onRouteChange} 
+                  loadUser={this.loadUser}
+                />
           )
         }
       </div>
